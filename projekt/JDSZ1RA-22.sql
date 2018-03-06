c95356fd05f34e42d1635cfab6d1761ff9061173
gornibar <gornibar@gmail.com>
2018-03-06 10:12:16 +0100

    JDSZ1RA-22

--- projekt/JDSZ1RA-22.sql
+++ projekt/JDSZ1RA-22.sql
@@ -1,66 +1,126 @@
 -- Ile leadów tworzonych jest każdego dnia dla każdego partnera?
-/*
--- EKSPLORACJA DANYCH
--- Ilu mam partnerów? 5 partnerów
-SELECT DISTINCT partner
-FROM m_dane_od_partnerow;
-
--- Jaki jest zakres dat dla leadów? Od 2015-10-26 13:06:52.907009, do 2998-01-01 00:00:00.000000
-SELECT
-  MIN(data_wysylki),
-  MAX(data_wysylki)
-FROM m_lead;
-
--- Skąd ta dziwna data? 17232 leadów z przyszłości. PR: To mogą być leady na zaplanowane kampanie.
-SELECT count(1)
-FROM m_lead
-WHERE data_wysylki > now();
-
--- Nie każda dana od partnerów ma datę wysyłki leada. Na 780397 danych od partnerów, 146065 ma datę wysylki leada , a 634332 jej nie ma.
-SELECT count(1)
-FROM m_dane_od_partnerow dp
-LEFT JOIN m_lead l ON dp.id = l.id
-WHERE l.data_wysylki IS NULL;
-
-
--- Data wysyłki leada jest najczęściej poźniejsza od daty kampanii,...
-SELECT COUNT(DISTINCT l.id)
-FROM m_lead l
-JOIN m_lead_kampania lk ON l.id = lk.id_lead
-JOIN m_kampanie k ON lk.id_kampania = k.id
-WHERE k.data_kampanii < l.data_wysylki
-ORDER BY 1;
-
--- ...a data utworzenia wniosku jest najczęściej wcześniejsza od daty utworzenia leada.
-SELECT COUNT(1)
-FROM m_lead l
-JOIN wnioski w ON l.id_wniosku = w.id
-WHERE l.data_wysylki < w.data_utworzenia;
-*/
-
--- ODPOWIEDŹ
+-- Bartosz Górnikiewicz
 -- Założenie: data wysyłki leada jest datą jego utworzenia,
-WITH leady AS (SELECT
-  TO_CHAR(l.data_wysylki, 'YYYY-MM-DD') data_utw_lead,
-  dop.partner partner,
-  COUNT(l.id) n_lead
-FROM m_lead l
--- dodajemy dane o partnerach dla kazdego leada
-JOIN m_dane_od_partnerow dop ON l.id = dop.id
--- dodajemy dane o wnioskach
-  LEFT JOIN wnioski w ON l.id_wniosku = w.id
--- bierzemy tylko leady, które zostały utworzone przed wnioskami
-WHERE l.data_wysylki < w.data_utworzenia OR w.data_utworzenia IS NULL
--- usuwamy leady utworzone w przyszłości
-  AND l.data_wysylki < now()
-GROUP BY 1, 2)
+WITH daty_graniczne AS (
+SELECT
+  MIN(data_wysylki) min_data,
+  MAX(data_wysylki) max_data
+FROM m_lead
+WHERE data_wysylki < NOW()),
 
+daty_wygenerowane AS (
+SELECT
+    GENERATE_SERIES(
+        (SELECT min_data FROM daty_graniczne)::date,
+        (SELECT max_data FROM daty_graniczne)::date,
+        '1 day'
+    )::date dzien),
+
+lead_na_dzien AS (
+      SELECT
+        TO_CHAR(l.data_wysylki, 'YYYY-MM-DD')::date dzien,
+        dop.partner partner,
+        COUNT(l.id) n_lead
+      FROM m_lead l
+        -- dodajemy dane o partnerach dla kazdego leada
+        JOIN m_dane_od_partnerow dop ON l.id = dop.id
+        -- dodajemy dane o wnioskach
+        LEFT JOIN wnioski w ON l.id_wniosku = w.id
+      -- bierzemy tylko leady, które zostały utworzone przed wnioskami
+      WHERE l.data_wysylki < w.data_utworzenia OR w.data_utworzenia IS NULL
+      -- usuwamy leady utworzone w przyszłości
+      AND l.data_wysylki < now()
+      GROUP BY 1, 2),
+
+daty_przystapienia AS (
 SELECT
   partner,
-  AVG(n_lead),
-  PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY n_lead),
-  PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY n_lead),
-  PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY n_lead)
-FROM leady
-GROUP BY partner;
+  MIN(dzien)::date data_przystapienia
+FROM lead_na_dzien
+GROUP BY 1),
+
+-- Wyciągamy liczbę leadów dal poszczególnych partnerów, aby móc przyporządkować 0 dniom bez leadów
+dreamtours AS (
+SELECT dzien, n_lead
+FROM lead_na_dzien
+WHERE partner = 'dreamtours'
+  ),
+
+dreamtours_corr AS (
+SELECT
+  dzien,
+  'dreamtours' partner,
+  COALESCE(dreamtours.n_lead, 0) n_lead
+FROM daty_wygenerowane dw
+LEFT JOIN dreamtours USING(dzien)
+WHERE dw.dzien >= (SELECT data_przystapienia FROM daty_przystapienia WHERE partner = 'dreamtours')),
+
+wakacje_pl AS (
+SELECT dzien, n_lead
+FROM lead_na_dzien
+WHERE partner = 'wakacje.pl'),
+
+wakacje_pl_corr AS (
+SELECT
+  dzien,
+  'wakacje.pl' partner,
+  COALESCE(wakacje_pl.n_lead, 0) n_lead
+FROM daty_wygenerowane dw
+LEFT JOIN wakacje_pl USING(dzien)
+WHERE dw.dzien >= (SELECT data_przystapienia FROM daty_przystapienia WHERE partner = 'wakacje.pl')),
+
+kiribati AS (
+SELECT dzien, n_lead
+FROM lead_na_dzien
+WHERE partner = 'kiribati'),
+
+kiribati_corr AS (
+SELECT
+  dzien,
+  'kiribati' partner,
+  COALESCE(kiribati.n_lead, 0) n_lead
+FROM daty_wygenerowane dw
+LEFT JOIN kiribati USING(dzien)
+WHERE dw.dzien >= (SELECT data_przystapienia FROM daty_przystapienia WHERE partner = 'kiribati')),
+
+tui AS (
+SELECT dzien, n_lead
+FROM lead_na_dzien
+WHERE partner = 'tui'),
+
+tui_corr AS (
+SELECT
+  dzien,
+  'tui' partner,
+  COALESCE(tui.n_lead, 0) n_lead
+FROM daty_wygenerowane dw
+LEFT JOIN tui USING(dzien)
+WHERE dw.dzien >= (SELECT data_przystapienia FROM daty_przystapienia WHERE partner = 'tui')),
+
+itaka AS (
+SELECT dzien, n_lead
+FROM lead_na_dzien
+WHERE partner = 'itaka'),
+
+itaka_corr AS (
+SELECT
+  dzien,
+  'itaka' partner,
+  COALESCE(itaka.n_lead, 0) n_lead
+FROM daty_wygenerowane dw
+LEFT JOIN itaka USING(dzien)
+WHERE dw.dzien >= (SELECT data_przystapienia FROM daty_przystapienia WHERE partner = 'itaka'))
+
+-- Następnie łączę zaytania. Uzywam UNION, a nie JOIN, bo łatwiej to później przeliczyć w Tableau.
+-- Bez okeślenia formatów dla każdego z pól SQL wyrzuca błąd.
+
+SELECT dzien::date, partner::text, n_lead::NUMERIC FROM dreamtours_corr
+UNION
+SELECT dzien::date, partner::text, n_lead::NUMERIC FROM wakacje_pl_corr
+UNION
+SELECT dzien::date, partner::text, n_lead::NUMERIC FROM kiribati_corr
+UNION
+SELECT dzien::date, partner::text, n_lead::NUMERIC FROM tui_corr
+UNION
+SELECT dzien::date, partner::text, n_lead::NUMERIC FROM itaka_corr;
 
