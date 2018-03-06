567e6b9109843182d86d1a7947ede1025470f0a2
monika.kucal <monika.kucal@gmail.com>
2018-03-06 19:35:36 +0100

    Wnioski przeterminowane update kampanie oczekujace/nowe MK

--- projekt/JDSZ1RA-23.sql
+++ projekt/JDSZ1RA-23.sql
@@ -17,11 +17,44 @@ leady_nieobjete_kampania as
 (select l.*, lk.id_kampania
 from m_lead l
 left join m_lead_kampania lk on l.id = lk.id_lead
-where lk.id_kampania is null)
+where lk.id_kampania is null),
 
-select ln.id id_lead, wd.id id_wniosku, wd.data_utworzenia, wd.data_wyjazdu, wd.max_termin, extract (days from wd.max_termin-now()) ile_dni_do_przeterminowania
+leady_objete_kampania_niewyslana as
+(select l.*, lk.id_kampania, k.status
+from m_lead l
+join m_lead_kampania lk on l.id = lk.id_lead
+  join m_kampanie k ON lk.id_kampania = k.id
+where k.status in ('oczekuje','nowa'))
+/*
+select coalesce(ln.id_kampania,lokn.id_kampania) id_kampania,
+  coalesce(ln.id,lokn.id) id_lead,
+  wd.id id_wniosku,
+  wd.data_utworzenia,
+  wd.data_wyjazdu,
+  wd.max_termin,
+  extract (days from wd.max_termin-now()) ile_dni_do_przeterminowania,
+  lokn.status
 from wnioski_max_termin wd
-join leady_nieobjete_kampania ln on (wd.id=ln.id_wniosku)
-where wd.max_termin>now()
-and  extract (days from wd.max_termin-now()) <120
+left join leady_nieobjete_kampania ln on (wd.id=ln.id_wniosku)
+left join leady_objete_kampania_niewyslana lokn on (wd.id=lokn.id_wniosku)
+where coalesce(ln.id,lokn.id) is not null
+and wd.max_termin>now()::timestamp
+and extract (days from wd.max_termin-now()::timestamp) <120
+order by 6;*/
+
+--do Tableau
+select coalesce(ln.id_kampania,lokn.id_kampania) id_kampania,
+  coalesce(ln.id,lokn.id) id_lead,
+  wd.id id_wniosku,
+  wd.data_utworzenia,
+  wd.data_wyjazdu,
+  wd.max_termin,
+  extract (days from wd.max_termin-now()) ile_dni_do_przeterminowania,
+  lokn.status
+from wnioski_max_termin wd
+left join leady_nieobjete_kampania ln on (wd.id=ln.id_wniosku)
+left join leady_objete_kampania_niewyslana lokn on (wd.id=lokn.id_wniosku)
+where coalesce(ln.id,lokn.id) is not null
+--and wd.max_termin>now()::timestamp
+and extract (days from wd.max_termin-now()::timestamp) <120
 order by 6;
