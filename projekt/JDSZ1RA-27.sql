66a219274fa02ee2a31e3c0b6f5b58bb4bb8cc4a
FJakubo <35261947+FJakubo@users.noreply.github.com>
2018-03-10 19:35:43 +0100

    Update JDSZ1RA-27.sql
    Poprawiona wersja

--- projekt/JDSZ1RA-27.sql
+++ projekt/JDSZ1RA-27.sql
@@ -2,21 +2,55 @@ Filip Jakubowski
 
 Ile emaili wysyłamy w dniu / miesiącu / kwartale / roku?
 jakie są łączne koszty wysyłki maili, jeśli wysłanie jednego emaila kosztuje 10groszy?
+czy są partnerzy, dla których nie opłaca nam sie wysyłać emaili (ponieważ bardzo mało spraw zostało wygranych)?
 
-select to_char(data_kampanii, 'YYYY') Rok,
-      to_char(data_kampanii, 'Month') Miesiace,
-      to_char(data_kampanii, 'Q') Kwartal,
-      to_char(data_kampanii, 'Day') Dni,
-      --count(4) ilosc_wyslanych_emaili,
-      count(CASE WHEN a.status IN ('zaakceptowany') then 1 END) ilosc_wyslanych_emaili,
-      --/sum(COUNT(7)) over (),5))*100 procent_zaakceptowanych,
-      round(count(CASE WHEN a.status IN ('zaakceptowany') then 1 END)*0.1,2) cena_wysania_emaili, -- cena wyslania maila to 10 gr
-      --count(CASE WHEN a.status IN ('zaakceptowany') then 1 END)*250*0.25 min_wartość_zysku -- minimalna rekompensata wynosi 250 i nasza prowizja to 25%
-FROM m_kampanie
-JOIN m_email m ON m_kampanie.id = m.id_kampanii
-JOIN m_lead_kampania m2 ON m_kampanie.id = m2.id_kampania
-JOIN m_lead l ON m2.id_lead = l.id
-JOIN wnioski w ON l.id_wniosku = w.id
-left JOIN analizy_wnioskow a ON w.id = a.id_wniosku
-GROUP by 1,2,3,4
-ORDER BY 1,3,2,4;
+
+WITH moje_dane AS (
+SELECT
+  TO_char(k.data_kampanii, 'YYYY-MM-DD') data_wyslania,
+  e.id,
+  dop.partner,
+  e.ile_otwarto otwarto,
+  e.ile_kliknieto klik,
+  w.kwota_rekompensaty_oryginalna kwota,
+  w.stan_wniosku stan_wniosku
+FROM m_lead l
+JOIN m_lead_kampania lk ON l.id = lk.id_lead
+JOIN m_kampanie k ON lk.id_kampania = k.id
+JOIN m_email e ON k.id = e.id_kampanii AND e.lead_id = l.id
+JOIN m_dane_od_partnerow dop ON l.id = dop.id
+LEFT JOIN wnioski w ON l.id_wniosku = w.id
+-- analizujemy tylko wysłane kampanie
+WHERE k.status = 'wyslane'
+-- oraz tylko wnioski utworzeone po lead'zie
+AND (l.data_wysylki < w.data_utworzenia OR w.data_utworzenia IS NULL)
+-- oraz odrzucamy leady z przyszłości
+AND l.data_wysylki < NOW()
+ORDER BY l.id),
+
+mail_partner_dzien AS (
+SELECT
+  data_wyslania,
+  partner,
+  COUNT(1) n_mail
+FROM moje_dane
+GROUP BY 1, 2),
+
+zarobione AS (
+SELECT
+  data_wyslania,
+  partner,
+  0.25*SUM(kwota) zarobek
+FROM moje_dane
+WHERE stan_wniosku = 'wyplacony'
+GROUP BY 1, 2
+ORDER BY 1)
+
+SELECT
+  mpd.data_wyslania,
+  mpd.partner,
+  mpd.n_mail,
+  zar.zarobek,
+  mpd.n_mail*0.1 koszt
+FROM mail_partner_dzien mpd
+JOIN zarobione zar ON zar.data_wyslania = mpd.data_wyslania AND zar.partner = mpd.partner;
